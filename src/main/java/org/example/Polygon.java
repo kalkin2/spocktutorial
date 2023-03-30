e164f0be91d110da0bf5f8df43e5e92c2c36c91a
kalkin <kalkin2@naver.com>
2023-03-30 23:32:48 +0900

    Mock test

--- src/main/java/org/example/Polygon.java
+++ src/main/java/org/example/Polygon.java
@@ -4,14 +4,28 @@ public class Polygon {
 
   private final int numberOfSides;
 
+  Renderer renderer;
+
   public Polygon(int numberOfSides) {
     if(numberOfSides <= 2){
       throw new TooFewSidesException("면의 갯수는 3개 이상어야 한다. ", numberOfSides);
     }
     this.numberOfSides = numberOfSides;
+    renderer = null;
+  }
+
+  public Polygon(Integer numberOfSides, Renderer renderer) {
+   this.numberOfSides = numberOfSides;
+   this.renderer = renderer;
   }
 
   public int getNumberOfSides() {
     return numberOfSides;
+  }
+
+  public void draw() {
+    for(int i = 0; i < numberOfSides; i++) {
+      renderer.drawLine();
+    }
   }
 }
---
+++ src/main/java/org/example/Renderer.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Renderer {
+
+  public void drawLine() {
+
+  }
+}
